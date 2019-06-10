1e176c5d5419c773d5b3c450a7b36a6b2a0c3dc5
Hong Baojin <13809735212@163.com>
2019-06-10 18:03:59 +0800

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+/* dom 加载完成 */
+$(function () {
+  /* 解决 banner 高度塌陷 */
+  $('.banner-bg').load(function(){ // 等图片加载完成
+    $('.banner').height($(this).height() - 38);
+  });
+
+  // 监听滚动
+  /*$(window).scroll(function () {
+    var scrollTop = $(this).scrollTop();
+    var scrollHeight = $(document).height();
+    var windowHeight = $(this).height();
+    if (scrollTop + windowHeight == scrollHeight) {
+      // alert("已经到最底部了！");
+      // 通过stop() 方法为被选元素停止当前正在运行的动画
+      $(".kit .footer").stop(true, true).slideDown();
+    } else {
+      $(".kit .footer").stop(true, true).slideUp();
+    }
+  });*/
+});
+
